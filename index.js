39ab47e42c2f4375fa5898f5dc7cf1df6fa0e590
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 02:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,17 +15,5 @@ function logWhisper(String){
 }
 
 function sayHiToGrandma (String){
-  var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
+  if(String.toLowerCase)
 }
