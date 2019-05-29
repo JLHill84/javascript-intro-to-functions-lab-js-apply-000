6145ca4d511cd1205ca0696ecd397e4ebf191950
Joshua Hill <josh@jlhworkshop.com>
2019-05-29 03:02:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-function shout(String){
+function shout(String) {
   return String.toUpperCase()
 }
 
-function whisper(String){
+function whisper(String) {
   return String.toLowerCase()
 }
 
-function logShout(String){
+function logShout(String) {
   console.log(String.toUpperCase())
 }
 
-function logWhisper(String){
+function logWhisper(String) {
   console.log(String.toLowerCase())
 }
 
-function sayHiToGrandma (String){
-  if(String.toLowerCase)
+function sayHiToGrandma (String) {
+  if(String.toLowerCase === String) {
+    return "I can't hear you!"
+  }
+  
+  if(String.toUpperCase === String) {
+    "YES INDEED!"
+  }
+  if(String === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
